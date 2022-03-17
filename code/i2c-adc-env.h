a0ce50649eae9073cac50afb466a7569ee38f2ac
Edward <eddykneller@gmail.com>
2022-03-17 13:32:45 -0700

    started i2cadc code

--- code/i2c-adc-env.h
+++ code/i2c-adc-env.h
@@ -14,16 +14,29 @@
 // ensure sys inclusions
 #include <xc.h>
 #include "sys-cfg-env.h"
+#include "i2c-env.h"
 
 // constant defs
-
+#define ADCADDRESS 0b10010110
 
 // global variables
 
 
 // func decs
+uint16_t mcp3021SingleSample(uint8_t);
+uint16_t mcp3021MultiSample(uint8_t);
 
+uint16_t mcp3021SingleSample(uint8_t rw) {
+    uint16_t word;
+    
+    i2cStart(); // send start bit
+    i2cTXByte(ADCADDRESS + rw); // transmit address byte
+    //word = i2cRXByte();
+}
 
+uint16_t mpc3021MultiSample() {
+    
+}
 #endif
 
 
